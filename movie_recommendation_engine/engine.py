0e234bf27338853f82248a217adce76e477b4c90
BigMurloc <dominik.pasymowski@gmail.com>
2023-11-16 16:17:52 +0100

    provide candidate name as user input

--- movie_recommendation_engine/engine.py
+++ movie_recommendation_engine/engine.py
@@ -30,23 +30,22 @@ def create_common_scoring(candidate, other_candidate, commonMovies):
     return np.array(candidate_recommendations, dtype=float), np.array(other_candidate_recommendations, dtype=float)
 
 
-chosen_candidate = find_candidate("Dominik Pasymowski")
-
-correlations = {}
-for other_candidate in candidates:
-    if other_candidate != chosen_candidate:
-        common_movies = find_common_movies(chosen_candidate, other_candidate)
-        chosen_candidate_scoring, other_candidate_scoring = create_common_scoring(
-            chosen_candidate,
-            other_candidate,
-            common_movies
-        )
-        if len(common_movies) > 2:
-            correlation, _ = pearsonr(chosen_candidate_scoring, other_candidate_scoring)
-            correlations.update({other_candidate.name: correlation})
-
-print(correlations)
-max_correlation = max(correlations, key=correlations.get)
-min_correlation = min(correlations, key=correlations.get)
-print(max_correlation)
-print(min_correlation)
+def find_recommended_movies_for_candidate(name):
+    chosen_candidate = find_candidate(name)
+    correlations = {}
+    for other_candidate in candidates:
+        if other_candidate != chosen_candidate:
+            common_movies = find_common_movies(chosen_candidate, other_candidate)
+            chosen_candidate_scoring, other_candidate_scoring = create_common_scoring(
+                chosen_candidate,
+                other_candidate,
+                common_movies
+            )
+            if len(common_movies) > 2:
+                correlation, _ = pearsonr(chosen_candidate_scoring, other_candidate_scoring)
+                correlations.update({other_candidate.name: correlation})
+    print(correlations)
+    max_correlation = max(correlations, key=correlations.get)
+    min_correlation = min(correlations, key=correlations.get)
+    print(max_correlation)
+    print(min_correlation)
---
+++ movie_recommendation_engine/main.py
@@ -0,0 +1,6 @@
+
+from engine import find_recommended_movies_for_candidate
+
+candidate_name = input("Enter your name: ")
+
+find_recommended_movies_for_candidate(candidate_name)
